a5b1cea9bff4d07d8475645912679a840727b73d
alsrb0332@naver.com <alsrb0332@naver,com>
2024-06-04 15:11:38 +0900

    fix comment

--- camera.py
+++ camera.py
@@ -4,19 +4,14 @@ import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
+        #OpenCV를 사용하여 장치 0에서 캡처합니다.
         self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
-        # Grab a single frame of video
+        # 비디오의 단일 프레임 캡처
         ret, frame = self.video.read()
         return frame
 
